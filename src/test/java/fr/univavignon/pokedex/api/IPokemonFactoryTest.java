caf2f44aa00d85a5cd9d2fbf510db3133892dbf4
kilian-quittard <kilian.quittard@alumni.univ-avignon.fr>
2023-04-15 19:45:29 +0200

    Début d'implémentation des tests pour la classe de la Team Rocket (TP6) + modification fichier config.yml (circleci)

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -16,7 +16,7 @@ public class IPokemonFactoryTest {
 
     @Before
     public void init() {
-        pokemonFactory = new RocketPokemonFactory();//new PokemonFactory();
+        pokemonFactory = new PokemonFactory();
         bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
         aquali = new Pokemon(133,"Aquali",186,168,260,2729,202,5000,4,100);
     }
