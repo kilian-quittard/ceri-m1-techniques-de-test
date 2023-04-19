ff4fd60067efb530d15b02e7588fe6d8efc2fe44
kilian-quittard <kilian.quittard@alumni.univ-avignon.fr>
2023-04-19 16:57:40 +0000

    Javadoc Updated

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++
@@ -1,9 +0,0 @@
-package fr.univavignon.pokedex.api;
-
-public class PokemonTrainerFactory implements IPokemonTrainerFactory{
-
-    @Override
-    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
-        return new PokemonTrainer(name,team,pokedexFactory.createPokedex(new PokemonMetadataProvider(),new PokemonFactory()));
-    }
-}
